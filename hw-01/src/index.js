929eda7fa5ae2d91efd9418493fd0cfc539612c5
Andrey Gonchar <umn1k.631@gmail.com>
2018-01-10 17:00:01 +0200

    Add comments

--- hw-01/src/index.js
+++ hw-01/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-//import Article from './Article'
+//Я немножко идиот) Чтобы посмотреть как и что работает нужно
+//что-то коментировать а что-то наоборот
+
+//Вывести несколько заметок
+//import Article from './Article' 
+
+//Сделать Привет мир который будет выводить ия из инпута
 //import App from './App';
+
+//Cкрывать и показывать дополнительную информацию
 import ContactList from './Contact/ContactList';
 
 
 ReactDOM.render(
+  //<Article />,
+  //<App />,
   <ContactList />, 
   document.getElementById('root'));
 registerServiceWorker();
